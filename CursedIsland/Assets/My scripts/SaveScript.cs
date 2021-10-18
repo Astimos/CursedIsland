4a89ce085007e32be576db6d6e52d58fb0b6013d
Astimos <48223590+Astimos@users.noreply.github.com>
2021-10-18 17:11:46 +0200

    + health,battery UI and Battery depletion system

---
+++ CursedIsland/Assets/My scripts/SaveScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveScript : MonoBehaviour
+{
+    public static int PlayerHealth = 100;
+    public static bool HealthChanged = false;
+    public static float BatteryPower = 1.0f;
+    public static bool FlashLightOn = false;
+    public static bool NVLightOn = false;
+
+}
