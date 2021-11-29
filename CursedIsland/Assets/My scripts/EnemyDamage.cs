dc82a2fdc8394c9a02ae2de9117f4f528aab66a7
Astimos <astimos5967@gmail.com>
2021-11-29 11:10:17 +0100

    + hit animation and helath

---
+++ CursedIsland/Assets/My scripts/EnemyDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public int EnemyHealth = 100;
+    private AudioSource MyPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Knife")) 
+        {
+            EnemyHealth -= 50;
+            MyPlayer.Play();
+        }
+    }
+}
